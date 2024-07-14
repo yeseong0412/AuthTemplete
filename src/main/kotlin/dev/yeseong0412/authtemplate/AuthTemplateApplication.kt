dd88662cb474f3fe55e065d28535a210aea9ea59
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-14 16:11:43 +0900

    init

---
+++ src/main/kotlin/dev/yeseong0412/authtemplate/AuthTemplateApplication.kt
@@ -0,0 +1,11 @@
+package dev.yeseong0412.authtemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/dev/yeseong0412/authtemplate/AuthTempleteApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yeseong0412.authtemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthTempleteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
